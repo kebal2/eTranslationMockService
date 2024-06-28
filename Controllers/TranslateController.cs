4303a43b51ff44192b5569524cc24b2c4cd24d14
Bálint Kelemen <kebal84@gmail.com>
2024-06-28 10:50:34 +0200

    Init commit

---
+++ Controllers/TranslateController.cs
@@ -0,0 +1,167 @@
+using System.Diagnostics;
+using System.Drawing;
+using System.Text;
+using System.Web;
+using System.Xml.Linq;
+
+using Microsoft.AspNetCore.Mvc;
+
+using Spire.Pdf;
+using Spire.Pdf.Graphics;
+
+using TranslateMock_dotnet.Services;
+
+namespace TranslateMock_dotnet.Controllers;
+
+[ApiController]
+[Route("/api/v1/[controller]")]
+public class TranslateController : ControllerBase
+{
+    private readonly ICallbackService callbackService;
+
+    public static readonly Random Rnd = new Random();
+
+    public TranslateController(ICallbackService callbackService)
+    {
+        this.callbackService = callbackService;
+    }
+
+    [HttpPost(Name = "translate")]
+    public string Post(TranslateRequest? requestData)
+    {
+        if (requestData is null) return "-30000";
+
+        var random = new Random();
+        var requestCode = "0";
+        var dests = Array.Empty<string>();
+        if (requestData.destinations is not null || !string.IsNullOrEmpty(requestData.requesterCallback)) dests = requestData.destinations?.httpDestinations ?? new[] { requestData.requesterCallback };
+
+        foreach (var destination in dests)
+        {
+            string content;
+            string decoded;
+
+            requestCode = random.Next(100000, int.MaxValue).ToString();
+
+            string format = "text";
+            if (requestData.documentToTranslateBase64 is not null)
+            {
+                content = requestData.documentToTranslateBase64.content;
+                decoded = content.FromBase64();
+                format = requestData.documentToTranslateBase64.format.ToLower();
+            }
+            else if (requestData.textToTranslate is not null)
+            {
+                decoded = content = requestData.textToTranslate;
+            }
+            else throw new InvalidOperationException();
+
+            switch (format)
+            {
+                case "text":
+                    content = $"{decoded} - should be translated to [{string.Join(", ", requestData.targetLanguages)}]";
+                    break;
+                case "html":
+                    content = $"{decoded}<h1>Should be translated to [{string.Join(", ", requestData.targetLanguages)}]</h1>".ToBase64();
+                    break;
+                case "xhtml":
+                {
+                    // ids mezők elemeinek ahol van tartalom adat hozáfűzése
+                    var xhtml = XElement.Parse(decoded, LoadOptions.PreserveWhitespace);
+
+                    foreach (var elem in xhtml.Elements())
+                    {
+                        elem.Value = $"{string.Join(", ", requestData.targetLanguages)} - {elem.Value}";
+                    }
+
+                    content = xhtml.ToString().ToBase64();
+
+                    break;
+                }
+
+                case "xml":
+                {
+                    // mezők tartalmához hozzáfűzni nyelvkódot
+                    var xml = XElement.Parse(decoded);
+
+                    foreach (var elem in xml.Elements())
+                    {
+                        elem.Value = $"{string.Join(", ", requestData.targetLanguages)} - {elem.Value}";
+                    }
+
+                    content = xml.ToString().ToBase64();
+
+                    break;
+                }
+                case "pdf":
+                {
+                    content = HandlePDF(requestData, decoded).ToBase64();
+                    break;
+                }
+                case "application/pdf":
+                {
+                    content = HandlePDF(requestData, decoded).ToBase64();
+                    break;
+                }
+                default:
+                    break;
+            }
+
+            Debug.WriteLine(content);
+
+            var waitTime = Rnd.Next(4000);
+            Debug.WriteLine($"Random time: {waitTime}");
+
+            Thread.Sleep(TimeSpan.FromMilliseconds(waitTime));
+
+            if (format == "text")
+                callbackService.AddDataToSend(new TextCallbackRequest(new Uri(destination), content, requestCode, requestData.externalReference, requestData.targetLanguages));
+            else
+                callbackService.AddDataToSend(new DocumentCallbackRequest(new Uri(destination), content, requestCode, requestData.externalReference, requestData.targetLanguages));
+        }
+
+        return requestCode;
+    }
+
+    private static byte[] HandlePDF(TranslateRequest requestData, string decoded)
+    {
+        string content;
+        var pdf = new PdfDocument(Encoding.ASCII.GetBytes(decoded));
+        PdfPageBase page = pdf.Pages.Add();
+
+        //Draw the text
+        page.Canvas.DrawString($"Hello, World! Translate to [{string.Join(", ", requestData.targetLanguages)}]",
+            new PdfFont(PdfFontFamily.Helvetica, 30f),
+            new PdfSolidBrush(Color.Black),
+            10, 10);
+
+        using var ms = new MemoryStream();
+
+        pdf.SaveToFile($"./test_{DateTime.Now.Ticks}.pdf");
+
+        pdf.SaveToStream(ms, FileFormat.DOCX);
+        System.IO.File.WriteAllBytes($"./test_{DateTime.Now.Ticks}.docx", ms.ToArray());
+
+        return ms.ToArray();
+    }
+}
+
+internal static class Base64Helper
+{
+    public static string ToBase64(this string text)
+    {
+        var base64String = Convert.ToBase64String(Encoding.UTF8.GetBytes(text));
+        return base64String;
+    }
+
+    public static string ToBase64(this byte[] data)
+    {
+        var base64String = Convert.ToBase64String(data, Base64FormattingOptions.InsertLineBreaks);
+        return base64String;
+    }
+
+    public static string FromBase64(this string text)
+    {
+        return Encoding.UTF8.GetString(Convert.FromBase64String(text));
+    }
+}
---
+++ Services/CallbackService.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections.Concurrent;
+using System.Text;
+using System.Web;
+
+namespace TranslateMock_dotnet.Services;
+
+public interface ICallbackRequest
+{
+    Uri Uri { get; }
+    string RequestCode { get; }
+    string? ExternalReference { get; }
+    string[] TargetLanguages { get; }
+}
+
+public record DocumentCallbackRequest(Uri Uri, string TranslatedDocumentsBase64, string RequestCode, string? ExternalReference, string[] TargetLanguages) : ICallbackRequest;
+
+public record TextCallbackRequest(Uri Uri, string TranslatedText, string RequestCode, string? ExternalReference, string[] TargetLanguages) : ICallbackRequest;
+
+
+
+public interface ICallbackService
+{
+    void AddDataToSend(DocumentCallbackRequest documentCallbackRequest);
+    void AddDataToSend(TextCallbackRequest textCallbackRequest);
+}
+
+public class CallbackService : ICallbackService, IDisposable
+{
+    private readonly IHttpClientFactory httpClientFactory;
+    private static readonly ConcurrentQueue<ICallbackRequest> DataToSend = new();
+
+    private bool running;
+    private bool disposing;
+    private bool isDisposed;
+
+    public CallbackService(IHttpClientFactory httpClientFactory)
+    {
+        this.httpClientFactory = httpClientFactory;
+
+        Task.Run(() => SendData());
+    }
+
+    public void AddDataToSend(DocumentCallbackRequest documentCallbackRequest)
+    {
+        DataToSend.Enqueue(documentCallbackRequest);
+    }
+
+    public void AddDataToSend(TextCallbackRequest textCallbackRequest)
+    {
+        DataToSend.Enqueue(textCallbackRequest);
+    }
+
+    private void SendData()
+    {
+        running = true;
+
+        while (!disposing && !isDisposed)
+        {
+            while (DataToSend.TryDequeue(out var callbackRequest))
+            {
+                using var httpClient = httpClientFactory.CreateClient();
+                foreach (var targetLanguage in callbackRequest.TargetLanguages)
+                {
+                    var query = HttpUtility.ParseQueryString(callbackRequest.Uri.Query);
+                    query["request-id"] = callbackRequest.RequestCode;
+
+                    if(!string.IsNullOrEmpty(callbackRequest.ExternalReference))
+                        query["external-reference"] = callbackRequest.ExternalReference;
+
+                    query["target-language"] = targetLanguage;
+
+                    StringContent sc = null;
+
+                    switch (callbackRequest)
+                    {
+                        case TextCallbackRequest tcr:
+                            query["translated-text"] = tcr.TranslatedText;
+                            break;
+                        case DocumentCallbackRequest dcr:
+                            sc = new StringContent(dcr.TranslatedDocumentsBase64, Encoding.UTF8);
+                            break;
+                    }
+
+                    UriBuilder uriBuilder = new(callbackRequest.Uri)
+                    {
+                        Query = query.ToString()
+                    };
+
+                    using HttpRequestMessage httpRequestMessage = new(HttpMethod.Post, uriBuilder.Uri);
+
+                    if (callbackRequest is DocumentCallbackRequest)
+                        httpRequestMessage.Content = sc;
+
+
+                    var response = httpClient.Send(httpRequestMessage);
+
+                    var result = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+                    Console.WriteLine($"Response from {callbackRequest.Uri}: {result}");
+                }
+            }
+
+            Thread.Sleep(1000);
+        }
+
+        running = false;
+    }
+
+    public void Dispose()
+    {
+        if (!disposing)
+        {
+            disposing = true;
+            int i = 5;
+            while (running && --i > 0)
+            {
+                Thread.Sleep(500);
+            }
+        }
+
+        isDisposed = true;
+    }
+}
